787f19f2ad42e903bc75067aea837db75412dc3b
vishakha <vishakhakasture302002@gmail.com>
2024-05-27 23:50:51 +0530

    initial commit

---
+++ netflix/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import Banner from "./Banner"
+import Nav from "./Nav";
+
+function App() {
+  return (
+    <div className="app">
+      {/* navbar */}
+      <Nav />
+      {/* Banner */}
+       <Banner />
+      {/* Rows*/} 
+    </div>
+      );
+}
+
+export default App; 
